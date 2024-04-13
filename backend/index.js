0e0948add837397d4749c75e01f03c2006147c1c
Harsh Shah <harshdev624@gmail.com>
2024-04-13 21:55:40 +0530

    Started Paytm Project

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+var cors = require("cors");
 
+const rootRouter = require("./routes/index");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/v1", rootRouter);
+
+app.listen(3000);
